7480114bd7a43821e5127ad1812e08ccc1433272
huangyuan <huangyuan.ws@inftech.ai>
2025-01-22 14:55:18 +0800

    update chat workflow

--- module/workflow/chat.go
+++ module/workflow/chat.go
@@ -3,7 +3,7 @@ package workflow
 import "context"
 
 func (w *Workflow) DemoChat(ctx context.Context, query, conversationId string) (string, string, error) {
-	token := "app-98ehTdEHEYn45dgKMuIOF6bf"
+	token := "app-cR1gm0hNz2cEgUBTwl9QPyqh"
 	inputs := map[string]string{}
 	return w.CallChatflowBlock(ctx, token, query, conversationId, inputs)
 }
